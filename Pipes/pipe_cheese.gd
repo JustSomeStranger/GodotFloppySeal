170ce675461b5e6bbb8618d7007c02a1ad86bde5
JustSomeStranger <justsomestranger23@gmail.com>
2024-08-16 00:59:36 +0200

    general content updates (sync commit)
    -new seal sprite
    -explosion effect
    -autospawning pipes
    maybe other stuff idk

--- Pipes/pipe_cheese.gd
+++ Pipes/pipe_cheese.gd
@@ -6,9 +6,7 @@ var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$".".hide()
-	queue_free()
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
-var velocity = 0
-var screen_size
-@export var JUMP_VELOCITY := 600.0
+signal player_died(player_position: Vector2)
+
+var dead := false
+var screen_size : Vector2
+var velocity := 0
+@export var JUMP_VELOCITY := 500.0
 @export var TERMINAL_VELOCITY := 1200.0
-@export var GRAVITY_STRENGTH := 35.0
+@export var GRAVITY_STRENGTH := 30.0
 @export var MAX_TILT := 0.4
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,10 +17,11 @@ func _ready():
 	var position = Vector2(150, 300)
 	$DebugLabel.show()
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity += GRAVITY_STRENGTH
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_just_pressed("jump"):
 		velocity = -JUMP_VELOCITY
 
 	velocity = clamp(velocity, -JUMP_VELOCITY, TERMINAL_VELOCITY)
@@ -27,3 +32,10 @@ func _process(delta):
 	#rotation = MAX_TILT * PI / 10 * velocity**(1.0/3.0) # Uses quadratic roots, doesn't work with negative numbers :(
 	
 	$DebugLabel.text = "Velocity " + str(velocity) + "\nRotation: " + str(rotation) + "\nScreen dimensions: " + str(screen_size)
+
+
+
+func die(area: Area2D) -> void:
+	if dead: return
+	#dead = true
+	player_died.emit(position)
